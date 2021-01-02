175825994a64f458c26249c3535a93e61596fa9f
3dsgirl09 <70934855+3dsgirl09@users.noreply.github.com>
2021-01-01 21:03:22 -0500

    Add files via upload

---
+++ anti-krnl.lua
@@ -0,0 +1,11 @@
+local Synapse = syn and true or false
+local Sirhurt = is_sirhurt_closure and true or false
+local Sentinel = secure_load and true or false
+local ProtoSmasher = pebc_execute and true or false
+local Krnl = KRNL_LOADED and true or false
+
+if Krnl then
+game.Players.LocalPlayer:Kick("No Krnl allowed SKID ~message from 3dsgirl09")
+else
+print("Good boi, You dont use Krnl ~message from a proud 3dsgirl09 :)")
+end
